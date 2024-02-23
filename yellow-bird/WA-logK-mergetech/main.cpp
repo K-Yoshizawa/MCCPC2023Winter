d5a4e528ffeecb6743d553dd316b6e99faee11d7
KStepping <ksteppingstone36@gmail.com>
2024-02-23 18:34:52 +0900

    マージテク（想定誤答）

---
+++ yellow-bird/WA-logK-mergetech/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N, M; cin >> N >> M;
+
+    set<int> now, ans;
+    for(int i = 0; i < M; ++i){
+        int t, x; cin >> t >> x;
+        if(t == 1) now.insert(x);
+        if(t == 2) now.erase(x);
+        if(t == 3){
+            if(now.size() > ans.size()){
+                for(auto x : ans) now.insert(x);
+                swap(now, ans);
+            }
+            else{
+                for(auto x : now) ans.insert(x);
+            }
+        }
+    }
+    cout << ans.size() << endl;
+    for(auto x : ans){
+        cout << x << " ";
+    }
+    cout << endl;
+}
