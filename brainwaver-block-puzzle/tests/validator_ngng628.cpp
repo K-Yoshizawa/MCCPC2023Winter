4a54bb0e5c3ce69ed6ba2839d3c256a31c4f4c9f
ngng628.kyopro@gmail.com <ngng628.kyopro@gmail.com>
2024-02-26 00:30:56 +0900

    :heavy_plus_sign: ngng628 の validator を追加

---
+++ brainwaver-block-puzzle/tests/validator_ngng628.cpp
@@ -0,0 +1,19 @@
+# include "testlib.h"
+# include <bits/stdc++.h>
+
+constexpr int MIN = -1e5;
+constexpr int MAX = 1e5;
+
+int main(int argc, char* argv[]){
+   registerValidation(argc, argv);
+   int sx = inf.readInt(MIN, MAX, "sx");
+   inf.readSpace();
+   int sy = inf.readInt(MIN, MAX, "sy");
+   inf.readSpace();
+   int gx = inf.readInt(MIN, MAX, "gx");
+   inf.readSpace();
+   int gy = inf.readInt(MIN, MAX, "gy");
+   inf.readEoln();
+   inf.readEof();
+   return 0;
+}
