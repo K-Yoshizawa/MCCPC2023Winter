27e82dec203451a07281c4b0452e78075d5a784e
new-textfile <newtextfile414@gmail.com>
2024-03-01 22:36:13 +0900

    validatorとtester解を追加

---
+++ tree-destroyer-game/AC-new_textfile/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+typedef long long int ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+#define rep(i,a,n) for(ll i = a; i < n; i++)
+
+int main(){
+    ll n;
+    cin >> n;
+
+    vvll G_undirected(n);
+    rep(i,0,n-1){
+        ll a, b;
+        cin >> a >> b;
+        a--; b--;
+        G_undirected[a].emplace_back(b);
+        G_undirected[b].emplace_back(a);
+    }
+
+    vll dist(n, 1ll << 60);
+    dist[0] = 0;
+    queue<ll> que;
+    que.emplace(0);
+    while(!que.empty()){
+        ll v = que.front();
+        que.pop();
+        
+        for(auto nv : G_undirected[v]){
+            if(dist[nv] > dist[v] + 1){
+                dist[nv] = dist[v] + 1;
+                que.emplace(nv);
+            }
+        }
+    }
+    
+    vvll G(n);
+    rep(v,0,n) for(auto nv : G_undirected[v]) if(dist[nv] > dist[v]) G[v].emplace_back(nv);
+
+    vector<bool> visited(n);
+    auto dfs = [&](ll v, auto &dfs) -> ll {
+        ll res = 1;
+        for(auto nv : G[v]){
+            if(!visited[nv]){
+                visited[nv] = true;
+                res += dfs(nv, dfs);
+            }
+        }
+
+        return res;
+    };
+
+    ll ans = 0;
+    rep(i,0,n-1){
+        ll x; cin >> x;
+        x--;
+        if(!visited[x]){
+            visited[x] = true;
+            ll res = dfs(x, dfs);
+            ans += res*res;
+        }
+    }
+
+    cout << ans << endl;
+}
+
---
+++ tree-destroyer-game/WA-new_textfile-overflow/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+typedef vector<int> vint;
+typedef vector<vint> vvint;
+
+#define rep(i,a,n) for(int i = a; i < n; i++)
+
+int main(){
+    int n;
+    cin >> n;
+
+    vvint G_undirected(n);
+    rep(i,0,n-1){
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        G_undirected[a].emplace_back(b);
+        G_undirected[b].emplace_back(a);
+    }
+
+    vint dist(n, 1 << 30);
+    dist[0] = 0;
+    queue<int> que;
+    que.emplace(0);
+    while(!que.empty()){
+        int v = que.front();
+        que.pop();
+        
+        for(auto nv : G_undirected[v]){
+            if(dist[nv] > dist[v] + 1){
+                dist[nv] = dist[v] + 1;
+                que.emplace(nv);
+            }
+        }
+    }
+    
+    vvint G(n);
+    rep(v,0,n) for(auto nv : G_undirected[v]) if(dist[nv] > dist[v]) G[v].emplace_back(nv);
+
+    vector<bool> visited(n);
+    auto dfs = [&](int v, auto &dfs) -> int {
+        int res = 1;
+        for(auto nv : G[v]){
+            if(!visited[nv]){
+                visited[nv] = true;
+                res += dfs(nv, dfs);
+            }
+        }
+
+        return res;
+    };
+
+    long long int ans = 0;
+    rep(i,0,n-1){
+        int x; cin >> x;
+        x--;
+        if(!visited[x]){
+            visited[x] = true;
+            auto res = dfs(x, dfs);
+            ans += res*res;
+        }
+    }
+
+    cout << ans << endl;
+}
+
