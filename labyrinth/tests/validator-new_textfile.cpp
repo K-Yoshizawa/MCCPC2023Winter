f520d9c885ea8b82b1af9164d88f9ab3e51188b3
new-textfile <newtextfile414@gmail.com>
2024-03-23 23:55:33 +0900

    制約変更に伴ってvalidatorを修正

--- labyrinth/tests/validator-new_textfile.cpp
+++ labyrinth/tests/validator-new_textfile.cpp
@@ -4,13 +4,13 @@
 const int MIN_N = 2;
 const int MAX_N = 700;
 const int MIN_M = 1;
-const int MAX_M = 1000000;
+//const int MAX_M = 1000000;
 
 int main(int argc, char* argv[]) {
     registerValidation(argc, argv);
     
     int N = inf.readInt(MIN_N, MAX_N, "N"); inf.readSpace();
-    int M = inf.readInt(MIN_M, MAX_M, "M"); inf.readEoln();
+    int M = inf.readInt(MIN_M, 2*N*N, "M"); inf.readEoln();
 
     std::string S = inf.readToken(format("[LRUD]{%d,%d}",M,M).c_str(), "S"); inf.readEoln();
     
