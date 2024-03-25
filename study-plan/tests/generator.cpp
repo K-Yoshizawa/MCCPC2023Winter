b9f4673a2582fa7cb42cd37a2db42f201a154a81
Ryohei Kobayashi <kobaryo222@gmail.com>
2024-03-25 19:44:56 +0900

    テストケースの強化

--- study-plan/tests/generator.cpp
+++ study-plan/tests/generator.cpp
@@ -29,6 +29,26 @@ void gen(string name, int N, int X, int Y, int Z, vector<int> A, vector<int> B, 
     ofs << endl;
 }
 
+template <class T>
+bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T>
+bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+bool check_ans_exists(int N, int X, int Y, int Z, vector<int>& A, vector<int>& B, vector<int>& C)
+{
+    vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(X + 1, vector<int>(Y + 1, 0)));
+    for (int i = 0; i < N; i++) {
+        for (int x = 0; x <= X; x++) {
+            for (int y = 0; y <= Y; y++) {
+                chmax(dp[i + 1][min(x + A[i], X)][y], dp[i][x][y]);
+                chmax(dp[i + 1][x][min(y + B[i], Y)], dp[i][x][y]);
+                chmax(dp[i + 1][x][y], dp[i][x][y] + C[i]);
+            }
+        }
+    }
+    return dp[N][X][Y] >= Z;
+}
+
 int main(int argc, char* argv[])
 {
     registerGen(argc, argv, 1);
@@ -56,6 +76,10 @@ int main(int argc, char* argv[])
             }
             gen(format("10_random_%02d.in", i + 1), N, X, Y, Z, A, B, C);
         }
+        gen("11_hand_01.in", 10, 10, 10, 10,
+            { 4, 4, 4, 2, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 3, 3, 3, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 3, 3, 4 });
     }
     // all
     {
@@ -78,11 +102,24 @@ int main(int argc, char* argv[])
             gen("21_max_01.in", N, X, Y, Z, A, B, C);
         }
         {
-            // max
+            // max - 1
             int N = MAX_N;
             int X = MAX_XYZ, Y = MAX_XYZ, Z = MAX_XYZ;
             vector<int> A(N, MAX_ABC - 1), B(N, MAX_ABC - 1), C(N, MAX_ABC - 1);
             gen("22_max_minus_1_01.in", N, X, Y, Z, A, B, C);
+        }
+        {
+            for (int i = 0; i < 10; i++) {
+                int N = MAX_N;
+                int X = MAX_XYZ, Y = MAX_XYZ, Z = MAX_XYZ;
+                vector<int> A(N, 1), B(N, 1), C(N, 1);
+                while (!check_ans_exists(N, X, Y, Z, A, B, C)) {
+                    A[rnd.next(N)]++;
+                    B[rnd.next(N)]++;
+                    C[rnd.next(N)]++;
+                }
+                gen(format("30_strong_%02d.in", i + 1), N, X, Y, Z, A, B, C);
+            }
         }
     }
     return 0;
